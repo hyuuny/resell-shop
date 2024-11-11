82632293579689690b9be1786772b6fcd7ab56f5
hyuuny <shyune@knou.ac.kr>
2024-11-11 21:35:25 +0900

    feat: init project

---
+++ src/main/kotlin/com/hyuuny/resellshop/ResellShopApplication.kt
@@ -0,0 +1,11 @@
+package com.hyuuny.resellshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResellShopApplication
+
+fun main(args: Array<String>) {
+    runApplication<ResellShopApplication>(*args)
+}
---
+++ src/test/kotlin/com/hyuuny/resellshop/ResellShopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hyuuny.resellshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ResellShopApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
