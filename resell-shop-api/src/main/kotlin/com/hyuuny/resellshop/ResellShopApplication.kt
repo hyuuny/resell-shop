073a868d708d11120760ffa0e751c0963118e4ce
hyuuny <shyune@knou.ac.kr>
2024-11-12 20:55:25 +0900

    refactor(main): main 메서드 @EnableJpaAuditing 제거

--- resell-shop-api/src/main/kotlin/com/hyuuny/resellshop/ResellShopApplication.kt
+++ resell-shop-api/src/main/kotlin/com/hyuuny/resellshop/ResellShopApplication.kt
@@ -2,9 +2,7 @@ package com.hyuuny.resellshop
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
-@EnableJpaAuditing
 @SpringBootApplication
 class ResellShopApplication
 
