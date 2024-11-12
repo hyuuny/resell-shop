802cb627fefea829ebdd2fbf2eff980126242359
hyuuny <shyune@knou.ac.kr>
2024-11-12 19:07:52 +0900

    refactor(main): main 메서드 api 모듈로 이동

--- resell-shop-api/src/main/kotlin/com/hyuuny/resellshop/ResellShopApplication.kt
+++ resell-shop-api/src/main/kotlin/com/hyuuny/resellshop/ResellShopApplication.kt
@@ -2,7 +2,9 @@ package com.hyuuny.resellshop
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableJpaAuditing
 @SpringBootApplication
 class ResellShopApplication
 
