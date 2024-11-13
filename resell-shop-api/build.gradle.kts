4764357a132ea19070a5a78b0a50319ec8a0aff3
hyuuny <shyune@knou.ac.kr>
2024-11-13 18:49:07 +0900

    build(gradle): query-dsl 의존성 추가

--- resell-shop-api/build.gradle.kts
+++ resell-shop-api/build.gradle.kts
@@ -16,4 +16,11 @@ dependencies {
 
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
+
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    implementation("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    implementation("jakarta.persistence:jakarta.persistence-api")
+    implementation("jakarta.annotation:jakarta.annotation-api")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
 }
