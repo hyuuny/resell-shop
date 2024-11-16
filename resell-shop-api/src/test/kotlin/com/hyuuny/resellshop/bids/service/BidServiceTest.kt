4df11844615c64f234e4eb0ee919f23d30b2b0ca
hyuuny <shyune@knou.ac.kr>
2024-11-16 11:16:12 +0900

    feat(bid): 구매자는 상품 구매 입찰을 등록할 수 있고, 입찰 히스토리도 저장된다

--- resell-shop-api/src/test/kotlin/com/hyuuny/resellshop/bids/service/BidServiceTest.kt
+++ resell-shop-api/src/test/kotlin/com/hyuuny/resellshop/bids/service/BidServiceTest.kt
@@ -51,4 +51,34 @@ class BidServiceTest(
         assertThat(savedBidHistory.status).isEqualTo(savedBid.status)
         assertThat(savedBidHistory.userId).isEqualTo(savedBid.userId)
     }
+
+    @Test
+    fun `구매자는 상품 구매 입찰을 등록할 수 있고, 입찰 히스토리도 저장된다`() {
+        val command = CreateBidCommand(
+            type = BidType.BUY,
+            userId = 2L,
+            productId = 1L,
+            productSizeId = 1L,
+            price = 50000,
+        )
+
+        val savedBid = service.create(command)
+        val savedBidHistory = bidHistoryRepository.findByBidId(savedBid.id)
+
+        assertThat(savedBid.id).isNotNull()
+        assertThat(savedBid.type).isEqualTo(command.type)
+        assertThat(savedBid.status).isEqualTo(BidStatus.WAITING)
+        assertThat(savedBid.orderNumber).isNotNull()
+        assertThat(savedBid.userId).isEqualTo(command.userId)
+        assertThat(savedBid.productId).isEqualTo(command.productId)
+        assertThat(savedBid.productSizeId).isEqualTo(command.productSizeId)
+        assertThat(savedBid.price).isEqualTo(command.price)
+        assertThat(savedBid.createdAt).isNotNull()
+
+        assertThat(savedBidHistory).isNotNull()
+        assertThat(savedBidHistory!!.bidId).isEqualTo(savedBid.id)
+        assertThat(savedBidHistory.type).isEqualTo(savedBid.type)
+        assertThat(savedBidHistory.status).isEqualTo(savedBid.status)
+        assertThat(savedBidHistory.userId).isEqualTo(savedBid.userId)
+    }
 }
