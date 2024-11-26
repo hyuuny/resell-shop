d5e14b6c7696420452ecb5720664ef93f35a0b28
hyuuny <shyune@knou.ac.kr>
2024-11-26 19:24:30 +0900

    refactor(bid): Reader내 사용하지 않는 메소드 제거

--- resell-shop-api/src/main/kotlin/com/hyuuny/resellshop/bids/infrastructure/BidReader.kt
+++ resell-shop-api/src/main/kotlin/com/hyuuny/resellshop/bids/infrastructure/BidReader.kt
@@ -1,20 +1,11 @@
 package com.hyuuny.resellshop.bids.infrastructure
 
 import com.hyuuny.resellshop.bids.domain.Bid
-import com.hyuuny.resellshop.bids.domain.BidStatus
-import com.hyuuny.resellshop.bids.domain.BidType
 import com.hyuuny.resellshop.bids.service.BidPriceDetailsResponse
 
 interface BidReader {
 
     fun findById(id: Long): Bid
-
-    fun findByTypeAndUserIdAndProductSizeIdAndStatusIn(
-        type: BidType,
-        userId: Long,
-        productSizeId: Long,
-        status: Collection<BidStatus>,
-    ): Bid?
 
     fun findAllMinPriceByProductSizeIdIn(productSizeIds: Collection<Long>): List<BidPriceDetailsResponse>
 
